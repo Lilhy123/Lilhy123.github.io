e36493c69fae99fadd4499d357aa790965c7d017
Lilhy <64278788+Lilhy123@users.noreply.github.com>
2022-01-18 10:13:12 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+function menus(){
+    let Desplazamiento_Actual = window.pageYOffset;
+
+    if(Desplazamiento_Actual <= 300){
+        
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+    }
+}
+
+
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+window.addEventListener('click',function(e){
+    console.log(e.target);
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+});
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
